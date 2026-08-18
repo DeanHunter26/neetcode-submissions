f3ec9a261c182d1962033ca36dc76629d5e41a68
Dean Hunter <140623219+DeanHunter26@users.noreply.github.com>
2026-08-18 14:30:32 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 1
+float_type = 3.43
+bool_type = True
+string_type = "bob"
+list_type = []
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
