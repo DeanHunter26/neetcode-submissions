e759a9f045830d0c1999d5b24b4c3401a9cf4802
Dean Hunter <140623219+DeanHunter26@users.noreply.github.com>
2026-08-18 14:31:17 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 1
+
+print(type(variable))
+
+variable = 2.3
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = "hi"
+
+print(type(variable))
+
+variable = []
+
+print(type(variable))
